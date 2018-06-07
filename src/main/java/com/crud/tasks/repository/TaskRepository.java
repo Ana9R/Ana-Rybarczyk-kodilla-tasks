d37fe9dd58d7d3d6710832597aa736bbf3fca91c
Ana9R <A.Burovac@interia.pl>
2018-06-07 22:49:21 +0200

    Implmented delete method

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,10 +2,22 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.yaml.snakeyaml.events.Event;
+import org.yaml.snakeyaml.tokens.Token;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface TaskRepository extends CrudRepository<Task, Long>{
+public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+
+    @Override
+    Task save(Task task);
+
+    @Override
+    Optional<Task> findById(Long id);
+
+    @Override
+    void deleteById (Long id);
 }
