519715f8e1ad5420d1e11eb5fabc121e4613f8d6
Ana9R <A.Burovac@interia.pl>
2018-07-04 23:31:09 +0200

    Json to java class

---
+++ src/main/java/com/crud/tasks/domain/AttachmentByType.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AttachmentByType {
+    private Trello trello;
+}
---
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Trello {
+    private Integer board;
+    private Integer card;
+}
